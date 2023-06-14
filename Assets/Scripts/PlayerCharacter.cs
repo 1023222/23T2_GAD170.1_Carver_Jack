4ee28545fef7faf03cd01c90bf12adc4286f1e0a
JC <136329097+1023222@users.noreply.github.com>
2023-06-14 16:06:06 +1000

    Added Gameplay Functionality
    Changed the variables to get values on start() rather than before.
    Added spacebar functionality directly related to volition variable.
    Created a (faux) win condition.

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -6,12 +6,12 @@ public class PlayerCharacter : MonoBehaviour
 {
     //Variables here!
     [SerializeField] private string playerName = "Nameless Fella";
-    [SerializeField] private int vitality = 100;
-    [SerializeField] private int physicalInstrument = 10;
-    [SerializeField] private int painThreshold = 10;
-    [SerializeField] private int endurance = 1;
-    [SerializeField] private int experience = 0;
-    [SerializeField] private int volition = 1;
+    [SerializeField] private int vitality;
+    [SerializeField] private int physicalInstrument;
+    [SerializeField] private int painThreshold;
+    [SerializeField] private int endurance;
+    [SerializeField] private int experience;
+    [SerializeField] private int volition;
 
     //Then Methods!
 
@@ -19,12 +19,29 @@ public class PlayerCharacter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
+        vitality = 100;
+        physicalInstrument = 10;
+        painThreshold = 10;
+        endurance = 1;
+        experience = 0;
+        volition = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+      // if the player presses the spacebar
+      if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //...do a thing!
+            Debug.Log("You know this working now dummy. ");
+            // ++ adds one to the variable.
+            volition++;
+        }
+
+      if(volition == 5)
+        {
+            Debug.Log("You Win");
+        }
     }
 }
